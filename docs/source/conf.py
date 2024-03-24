f7d2914fca95db72fc7cb180454fff16d4f9bf95
Christine1230 <159147763+Christine1230@users.noreply.github.com>
2024-03-24 20:58:55 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -48,3 +48,9 @@ source_parsers = {
 }
 
 source_suffix = ['.rst', '.md']
+
+from recommonmark.parser import CommonMarkParser
+source_parsers = {
+ '.md': CommonMarkParser,
+}
+source_suffix = ['.rst', '.md']
